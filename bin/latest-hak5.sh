e88265296ff750c8495f71514fc2d690000d8a7a
papamoose <philk1989@gmail.com>
2013-03-08 11:08:34 -0600

    Adding season 14

--- bin/latest-hak5.sh
+++ bin/latest-hak5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # hak5 weekly
-dldir="/storage/video/podcasts/Hak5/S12"
+dldir="/storage/video/podcasts/Hak5/S14"
 sdir="/storage/video/podcasts/Hak5"
 wget -O $sdir/weeklyfeed "http://revision3.com/hak5/feed/MP4-hd30?subshow=false"
 filename=`cat $sdir/weeklyfeed | grep "mp4" | egrep -v '<guid|<atom|<itunes' | sed 's/^.*url="//' | sed 's/".*$//' | uniq | head -n1`
